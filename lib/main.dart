795c8250dcc47915d029db477055a3e7dfd04ce2
Ade Prisma <ade@dinamika.ac.id>
2024-12-12 21:52:59 +0700

    Revert "day three"
    This reverts commit 540bd60ef85d95972ee8040bc21875160f4ae9cd.

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
