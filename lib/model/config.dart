038143a375992ff4edb641ecb3fe4c3cb82cedb5
Ade Prisma <ade@dinamika.ac.id>
2024-12-12 21:55:04 +0700

    Revert "Revert "day three""
    This reverts commit 795c8250dcc47915d029db477055a3e7dfd04ce2.

--- lib/model/config.dart
+++ lib/model/config.dart
@@ -5,5 +5,22 @@ class Config {
 
   Config._internal();
 
-  final String baseUrl = "https://b506-202-51-113-148.ngrok-free.app/api";
+  final String baseUrl = "http://backend-bootcamp.localhost/api";
+
+  String formatCurrency(int value) {
+    // Convert the number to a string and split it into the integer and decimal parts
+    String stringValue = value.toStringAsFixed(0); // No decimal points
+    String result = '';
+    int count = 0;
+    // Iterate backward through the string to insert commas
+    for (int i = stringValue.length - 1; i >= 0; i--) {
+      result = stringValue[i] + result;
+      count++;
+      if (count == 3 && i != 0) {
+        result = '.' + result;
+        count = 0;
+      }
+    }
+    return 'Rp' + result;
+  }
 }
